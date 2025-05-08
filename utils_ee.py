c81e1691801e1a912454a82f026449faee7781ee
desirmed <desmond.lartey@deltares.nl>
2025-05-08 15:22:08 +0200

    V

--- utils_ee.py
+++ utils_ee.py
@@ -1,14 +1,25 @@
 import json
 import ee
 import streamlit as st
+from google.oauth2 import service_account
 
 def initialize_earth_engine():
     try:
+        # Retrieve the JSON credentials from Streamlit secrets
         key_dict = json.loads(st.secrets["GEE_KEY"])
-        credentials = ee.ServiceAccountCredentials(
-            key_dict["client_email"], key_data=key_dict
+        
+        # Add custom scopes
+        scopes = ['https://www.googleapis.com/auth/earthengine', 
+                  'https://www.googleapis.com/auth/devstorage.read_write']
+        
+        # Create credentials object with specified scopes
+        credentials = service_account.Credentials.from_service_account_info(
+            key_dict, scopes=scopes
         )
+        
+        # Initialize Earth Engine with the credentials
         ee.Initialize(credentials)
+        
         st.success("Earth Engine initialized successfully!")
     except Exception as e:
         st.error(f"Failed to initialize Earth Engine: {e}")
