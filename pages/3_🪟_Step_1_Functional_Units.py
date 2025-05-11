03d73b889df431d86934eac91c86bf086a4df813
Desmond Lartey <larteydesmond3@gmail.com>
2025-05-11 21:32:20 +0200

    v

--- pages/3_🪟_Step_1_Functional_Units.py
+++ pages/3_🪟_Step_1_Functional_Units.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import leafmap.maplibregl as maplibre
+import os
+
+# Set MapTiler API Key for 3D terrain
+os.environ["MAPTILER_KEY"] = "iiyRi7eIx4NmHrMOEZsc"
 
 st.set_page_config(layout="wide")
 
@@ -10,14 +15,30 @@ st.sidebar.info(
     """
 )
 
-st.title("Split-panel Map")
+st.title("Split-panel Map with 3D Visualization")
 
-with st.expander("See source code"):
-    with st.echo():
-        m = leafmap.Map()
-        m.split_map(
-            left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
-        )
-        m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+# Split layout into two columns for side-by-side maps
+col1, col2 = st.columns(2)
 
-m.to_streamlit(height=700)
+with col1:
+    st.subheader("2D Split Panel Map")
+    with st.expander("See source code for 2D map"):
+        with st.echo():
+            m = leafmap.Map()
+            m.split_map(
+                left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
+            )
+            m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+    m.to_streamlit(height=600)
+
+with col2:
+    st.subheader("3D Globe Visualization")
+
+    with st.expander("See source code for 3D map"):
+        with st.echo():
+            m3d = maplibre.Map(style="3d-terrain", projection="globe")
+            m3d.add_basemap("Esri.WorldImagery")
+            m3d.add_overture_3d_buildings()
+            m3d.add_layer_control()
+
+    m3d.to_streamlit(height=600)
