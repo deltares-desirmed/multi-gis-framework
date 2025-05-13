21caf79a899d352e366403341f71a77d2488463a
Desmond Lartey <larteydesmond3@gmail.com>
2025-05-13 11:43:29 +0200

    v

--- pages/1_🪟_Step_1_Functional_Units.py
+++ pages/1_🪟_Step_1_Functional_Units.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import ee
-import leafmap.foliumap as leafmap
 import folium
-from utils_ee import initialize_earth_engine
+import leafmap.foliumap as leafmap
+from utils_ee import initialize_earth_engine  #  Auth from secret config
 
-#  Initialize Earth Engine Using Cloud Secrets
-st.set_page_config(layout="wide")
+#  Initialize Earth Engine
 initialize_earth_engine()
 
-# Streamlit Sidebar
+st.set_page_config(layout="wide")
 st.sidebar.title("Info")
 st.sidebar.info(
     """
@@ -17,125 +16,129 @@ st.sidebar.info(
     """
 )
 
-st.title("Functional Units-cORINE 2018")
+st.title("CORINE Land Cover 2018 Visualization via Google Earth Engine")
 
-#  Register Custom Method to Add EE Tile Layers
-def add_ee_tile_layer(self, ee_image_object, vis_params, name):
-    map_id_dict = ee_image_object.getMapId(vis_params)
-    folium.TileLayer(
-        tiles=map_id_dict["tile_fetcher"].url_format,
-        attr="Google Earth Engine",
-        name=name,
-        overlay=True,
-        control=True
-    ).add_to(self)
+#  Add EE tile layer registration
+folium.Map.add_ee_tile_layer = lambda self, ee_img, vis_params, name: folium.TileLayer(
+    tiles=ee_img.getMapId(vis_params)["tile_fetcher"].url_format,
+    attr="Google Earth Engine",
+    name=name,
+    overlay=True,
+    control=True
+).add_to(self)
 
-folium.Map.add_ee_tile_layer = add_ee_tile_layer
+#  Control code visibility
+show_code = False
 
-with st.expander("See source code"):
-    with st.echo():
-        m = leafmap.Map(center=[50, 10], zoom=5)
-
-        corine = ee.Image("COPERNICUS/CORINE/V20/100m/2018")
-
-        vis_params = {
-            "bands": ["landcover"],
-            "min": 111,
-            "max": 523,
-            "palette": [
-                "#e6004d", "#ff0000", "#cc4df2", "#cc0000", "#e6cccc", "#e6cce6", 
-                "#a600cc", "#a64dcc", "#ff4dff", "#ffa6ff", "#ffe6ff", "#ffffa8", 
-                "#ffff00", "#e6e600", "#e68000", "#f2a64d", "#e6a600", "#e6e64d", 
-                "#ffe6a6", "#ffe64d", "#e6cc4d", "#f2cca6", "#80ff00", "#00a600", 
-                "#4dff00", "#ccf24d", "#a6ff80", "#a6e64d", "#a6f200", "#e6e6e6", 
-                "#cccccc", "#ccffcc", "#000000", "#a6e6cc", "#a6a6ff", "#4d4dff", 
-                "#ccccff", "#e6e6ff", "#a6a6e6", "#00ccf2", "#80f2e6", "#00ffa6", 
-                "#a6ffe6", "#e6f2ff"
-            ]
-        }
-
-        m.add_ee_tile_layer(corine, vis_params, "CORINE Land Cover 2018")
-
-        #  Create Legend HTML Content
-        labels = [
-            "Continuous urban fabric", "Discontinuous urban fabric", "Industrial/Commercial units",
-            "Road and rail networks", "Port areas", "Airports", "Mineral extraction sites",
-            "Dump sites", "Construction sites", "Green urban areas", "Sport/leisure facilities",
-            "Non-irrigated arable land", "Permanently irrigated land", "Rice fields",
-            "Vineyards", "Fruit trees and berry plantations", "Olive groves", "Pastures",
-            "Annual crops with permanent crops", "Complex cultivation patterns",
-            "Agriculture + natural areas", "Agro-forestry areas", "Broad-leaved forest",
-            "Coniferous forest", "Mixed forest", "Natural grassland", "Moors and heathland",
-            "Sclerophyllous vegetation", "Transitional woodland-shrub", "Beaches, dunes, sands",
-            "Bare rocks", "Sparsely vegetated areas", "Burnt areas", "Glaciers and perpetual snow",
-            "Inland wetlands", "Peat bogs", "Salt marshes", "Salines", "Intertidal flats",
-            "Water courses", "Water bodies", "Coastal lagoons", "Estuaries", "Sea and ocean"
-        ]
-
-        colors = [
-            "#ff0000", "#e97419", "#a00000", "#ffff64", "#009900", "#006400",
-            "#00ff00", "#00a000", "#dcdcdc", "#c8c8c8", "#ff0000", "#ffffa8",
+if show_code:
+    with st.expander("See source code", expanded=False):
+        with st.echo():
+            m = leafmap.Map(center=[50, 10], zoom=5)
+            corine = ee.Image("COPERNICUS/CORINE/V20/100m/2018")
+            vis_params = {
+                "bands": ["landcover"],
+                "min": 111,
+                "max": 523,
+                "palette": [
+                    "#e6004d", "#ff0000", "#cc4df2", "#cc0000", "#e6cccc", "#e6cce6",
+                    "#a600cc", "#a64dcc", "#ff4dff", "#ffa6ff", "#ffe6ff", "#ffffa8",
+                    "#ffff00", "#e6e600", "#e68000", "#f2a64d", "#e6a600", "#e6e64d",
+                    "#ffe6a6", "#ffe64d", "#e6cc4d", "#f2cca6", "#80ff00", "#00a600",
+                    "#4dff00", "#ccf24d", "#a6ff80", "#a6e64d", "#a6f200", "#e6e6e6",
+                    "#cccccc", "#ccffcc", "#000000", "#a6e6cc", "#a6a6ff", "#4d4dff",
+                    "#ccccff", "#e6e6ff", "#a6a6e6", "#00ccf2", "#80f2e6", "#00ffa6",
+                    "#a6ffe6", "#e6f2ff"]
+            }
+            m.add_ee_tile_layer(corine, vis_params, "CORINE Land Cover 2018")
+else:
+    m = leafmap.Map(center=[50, 10], zoom=5)
+    corine = ee.Image("COPERNICUS/CORINE/V20/100m/2018")
+    vis_params = {
+        "bands": ["landcover"],
+        "min": 111,
+        "max": 523,
+        "palette": [
+            "#e6004d", "#ff0000", "#cc4df2", "#cc0000", "#e6cccc", "#e6cce6",
+            "#a600cc", "#a64dcc", "#ff4dff", "#ffa6ff", "#ffe6ff", "#ffffa8",
             "#ffff00", "#e6e600", "#e68000", "#f2a64d", "#e6a600", "#e6e64d",
             "#ffe6a6", "#ffe64d", "#e6cc4d", "#f2cca6", "#80ff00", "#00a600",
             "#4dff00", "#ccf24d", "#a6ff80", "#a6e64d", "#a6f200", "#e6e6e6",
             "#cccccc", "#ccffcc", "#000000", "#a6e6cc", "#a6a6ff", "#4d4dff",
             "#ccccff", "#e6e6ff", "#a6a6e6", "#00ccf2", "#80f2e6", "#00ffa6",
-            "#a6ffe6", "#e6f2ff"
-        ]
+            "#a6ffe6", "#e6f2ff"]
+    }
+    m.add_ee_tile_layer(corine, vis_params, "CORINE Land Cover 2018")
 
-        legend_html = """
-        <div id="map-legend" style="
-            position: fixed; 
-            bottom: 50px; left: 50px; width: 300px; height: 400px; 
-            overflow: auto; 
-            background-color: white; 
-            border:2px solid grey; 
-            z-index:9999; 
-            font-size:14px;
-            padding: 10px;
-            display: none;">
-        <b>CORINE Land Cover 2018</b><br>
-        <hr>
-        """
+#  Legend content
+labels = [
+    "Continuous urban fabric", "Discontinuous urban fabric", "Industrial/Commercial units",
+    "Road and rail networks", "Port areas", "Airports", "Mineral extraction sites",
+    "Dump sites", "Construction sites", "Green urban areas", "Sport/leisure facilities",
+    "Non-irrigated arable land", "Permanently irrigated land", "Rice fields",
+    "Vineyards", "Fruit trees and berry plantations", "Olive groves", "Pastures",
+    "Annual crops with permanent crops", "Complex cultivation patterns",
+    "Agriculture + natural areas", "Agro-forestry areas", "Broad-leaved forest",
+    "Coniferous forest", "Mixed forest", "Natural grassland", "Moors and heathland",
+    "Sclerophyllous vegetation", "Transitional woodland-shrub", "Beaches, dunes, sands",
+    "Bare rocks", "Sparsely vegetated areas", "Burnt areas", "Glaciers and perpetual snow",
+    "Inland wetlands", "Peat bogs", "Salt marshes", "Salines", "Intertidal flats",
+    "Water courses", "Water bodies", "Coastal lagoons", "Estuaries", "Sea and ocean"]
 
-        for label, color in zip(labels, colors):
-            legend_html += f'<i style="background:{color};width:10px;height:10px;float:left;margin-right:8px;"></i>{label}<br>'
+colors = [
+    "#ff0000", "#e97419", "#a00000", "#ffff64", "#009900", "#006400",
+    "#00ff00", "#00a000", "#dcdcdc", "#c8c8c8", "#ff0000", "#ffffa8",
+    "#ffff00", "#e6e600", "#e68000", "#f2a64d", "#e6a600", "#e6e64d",
+    "#ffe6a6", "#ffe64d", "#e6cc4d", "#f2cca6", "#80ff00", "#00a600",
+    "#4dff00", "#ccf24d", "#a6ff80", "#a6e64d", "#a6f200", "#e6e6e6",
+    "#cccccc", "#ccffcc", "#000000", "#a6e6cc", "#a6a6ff", "#4d4dff",
+    "#ccccff", "#e6e6ff", "#a6a6e6", "#00ccf2", "#80f2e6", "#00ffa6",
+    "#a6ffe6", "#e6f2ff"]
 
-        legend_html += "</div>"
+legend_html = """
+<div id=\"map-legend\" style="
+    position: fixed; 
+    bottom: 50px; left: 50px; width: 300px; height: 400px; 
+    overflow: auto; 
+    background-color: white; 
+    border:2px solid grey; 
+    z-index:9999; 
+    font-size:14px;
+    padding: 10px;
+    display: none;">
+<b>CORINE Land Cover 2018</b><br>
+<hr>
+"""
+for label, color in zip(labels, colors):
+    legend_html += f'<i style="background:{color};width:10px;height:10px;float:left;margin-right:8px;"></i>{label}<br>'
+legend_html += "</div>"
+m.get_root().html.add_child(folium.Element(legend_html))
 
-        # Inject Legend HTML
-        m.get_root().html.add_child(folium.Element(legend_html))
-
-        # Inject JavaScript to Control Legend Visibility via Layer Control
-        js_toggle = """
-        <script>
-        var legend = document.getElementById('map-legend');
-        var checkExist = setInterval(function() {
-            var layers = document.querySelectorAll('.leaflet-control-layers-selector');
-            layers.forEach(function(layer) {
-                if (layer.nextSibling && layer.nextSibling.textContent.includes('Legend')) {
-                    layer.addEventListener('change', function() {
-                        if (layer.checked) {
-                            legend.style.display = 'block';
-                        } else {
-                            legend.style.display = 'none';
-                        }
-                    });
-                }
+# Add toggle functionality
+toggle_script = """
+<script>
+var legend = document.getElementById('map-legend');
+var checkExist = setInterval(function() {
+    var layers = document.querySelectorAll('.leaflet-control-layers-selector');
+    layers.forEach(function(layer) {
+        if (layer.nextSibling && layer.nextSibling.textContent.includes('Legend')) {
+            layer.addEventListener('change', function() {
+                legend.style.display = layer.checked ? 'block' : 'none';
             });
-            if (layers.length > 0) clearInterval(checkExist);
-        }, 500);
-        </script>
-        """
-        m.get_root().html.add_child(folium.Element(js_toggle))
+        }
+    });
+    if (layers.length > 0) clearInterval(checkExist);
+}, 500);
+</script>
+"""
+m.get_root().html.add_child(folium.Element(toggle_script))
 
-        # Add Legend as a Dummy Layer to Appear in the Layer Control
-        legend_layer = folium.FeatureGroup(name="Legend", show=False)
-        m.add_child(legend_layer)
+# Dummy layer to toggle legend
+legend_layer = folium.FeatureGroup(name="Legend", show=False)
+m.add_child(legend_layer)
 
-        # Finalize Map Display
-        m.add_layer_control()
-        m.to_streamlit(height=700)
+# Add controls and show map
+m.add_layer_control()
+m.to_streamlit(height=700)
 
 
 
