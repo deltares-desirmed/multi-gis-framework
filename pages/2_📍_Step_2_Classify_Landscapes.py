06b2c414007f9088bdf515716e45adbe2698ea50
Desmond Lartey <larteydesmond3@gmail.com>
2025-05-13 09:40:38 +0200

    v

--- pages/2_📍_Step_2_Classify_Landscapes.py
+++ pages/2_📍_Step_2_Classify_Landscapes.py
@@ -27,7 +27,7 @@ st.title("Landscape Characters - 3-domain")
 github_base_url = "https://raw.githubusercontent.com/deltares-desirmed/multi-gis-framework/main/database/"
 
 # 📚 List all GeoJSON files dynamically (Hardcoded here, but can be automated via GitHub API)
-geojson_files = ["EU_healthservices.geojson", "EU_education_filtered.geojson"]  # Add new files here or automate!
+geojson_files = ["EU_healthservices.geojson", "EU_education.geojson"]  # Add new files here or automate!
 
 # 🌍 Create Map Centered on Europe
 m = leafmap.Map(center=[50, 10], zoom=5) 
