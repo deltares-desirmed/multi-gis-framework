b3fc3d11b07e301853bc414f744c0912c706e80c
Desmond Lartey <larteydesmond3@gmail.com>
2025-05-13 11:45:16 +0200

    v

--- pages/1_🪟_Step_1_Functional_Units.py
+++ pages/1_🪟_Step_1_Functional_Units.py
@@ -152,6 +152,4 @@ m.to_streamlit(height=700)
 #             left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
 #         )
 #         m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
-
-
-m.to_streamlit(height=700)
+# m.to_streamlit(height=700)
