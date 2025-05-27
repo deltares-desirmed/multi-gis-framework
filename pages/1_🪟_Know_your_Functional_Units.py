dce71aa82a825f5831132aeaddca129cffeca98f
Desmond Lartey <larteydesmond3@gmail.com>
2025-05-27 22:04:57 +0200

    v

--- pages/1_🪟_Know_your_Functional_Units.py
+++ pages/1_🪟_Know_your_Functional_Units.py
@@ -103,9 +103,9 @@ archetype_img = reclassify(corine_img).clip(final_aoi)
 # Map Display
 st.subheader(f"Reclassified Landscape Archetypes ({selected_year})")
 Map = geemap.Map(center=[51, 3], zoom=8)
-Map.addLayer(final_aoi.style(**{
-    "color": "red", "fillColor": "00000000", "width": 2
-}), {}, "AOI Boundary")
+# Map.addLayer(final_aoi.style(**{
+#     "color": "red", "fillColor": "00000000", "width": 2
+# }), {}, "AOI Boundary")
 
 Map.addLayer(archetype_img, {"min": 1, "max": 14, "palette": palette}, f"Archetypes {selected_year}")
 # Create the map
