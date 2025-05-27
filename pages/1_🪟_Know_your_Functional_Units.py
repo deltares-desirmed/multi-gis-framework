35df833e8b32ab6004b93bf3a1ef1cdc110291e3
Desmond Lartey <larteydesmond3@gmail.com>
2025-05-27 22:35:39 +0200

    v

--- pages/1_🪟_Know_your_Functional_Units.py
+++ pages/1_🪟_Know_your_Functional_Units.py
@@ -123,9 +123,9 @@ Map = geemap.Map(center=[51, 3], zoom=8)
 # Map.addLayer(final_aoi.style(**{
 #     "color": "red", "fillColor": "00000000", "width": 2
 # }), {}, "AOI Boundary")
-Map.addLayer(archetype_img, {
-            "min": 1, "max": 14, "palette": palette
-        }, f"Archetypes {selected_year}")
+# Map.addLayer(archetype_img, {
+#             "min": 1, "max": 14, "palette": palette
+#         }, f"Archetypes {selected_year}")
 # Map.addLayer(archetype_img, {"min": 1, "max": 14, "palette": palette}, f"Archetypes {selected_year}")
 # ✅ Only run map logic if final_aoi is valid
 if final_aoi:
