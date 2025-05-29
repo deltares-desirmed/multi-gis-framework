f89c113eeadd8b0910fcc547f6366b2dcc32f5c2
Desmond Lartey <larteydesmond3@gmail.com>
2025-05-29 21:39:00 +0200

    v

--- pages/3_🌲_Step_2b_CRics.py
+++ pages/3_🌲_Step_2b_CRics.py
@@ -29,29 +29,30 @@ floods_hp_img = rasterize_floods(floods_HP)
 floods_mp_img = rasterize_floods(floods_MP)
 floods_lp_img = rasterize_floods(floods_LP)
 
-# Visualization palette for flood depths
+# Flood visualization
 flood_palette = ['blue', 'cyan', 'yellow', 'orange', 'red']
-flood_vis_params = {"min": 1, "max": 5, "palette": flood_palette}
+flood_vis = {"min": 1, "max": 5, "palette": flood_palette}
 
 # Create tile layers for floods
-hp_layer = geemap.ee_tile_layer(floods_hp_img, flood_vis_params, "Floods HP")
-mp_layer = geemap.ee_tile_layer(floods_mp_img, flood_vis_params, "Floods MP")
-lp_layer = geemap.ee_tile_layer(floods_lp_img, flood_vis_params, "Floods LP")
+hp_layer = geemap.ee_tile_layer(floods_hp_img, flood_vis, "Floods HP")
+mp_layer = geemap.ee_tile_layer(floods_mp_img, flood_vis, "Floods MP")
+lp_layer = geemap.ee_tile_layer(floods_lp_img, flood_vis, "Floods LP")
 
+# Load other base datasets
+esa = ee.ImageCollection("ESA/WorldCover/v100").first()
+esa_vis = {"bands": ["Map"]}
 
-# Add to dictionary for selection
-flood_layers = {
-    "Floods HP": hp_layer,
-    "Floods MP": mp_layer,
-    "Floods LP": lp_layer,
+esri = ee.ImageCollection("projects/sat-io/open-datasets/landcover/ESRI_Global-LULC_10m").mosaic()
+esri_vis = {
+    "min": 1,
+    "max": 10,
+    "palette": [
+        "#1A5BAB", "#358221", "#A7D282", "#87D19E", "#FFDB5C",
+        "#EECFA8", "#ED022A", "#EDE9E4", "#F2FAFF", "#C8C8C8",
+    ],
 }
 
-# Let user add one flood layer to the map
-flood_layer_choice = st.selectbox("Select flood layer to display", list(flood_layers.keys()), index=0)
-
-
-
-
+# UI Sidebar Info
 st.sidebar.title("Info")
 st.sidebar.info(
     """
@@ -59,100 +60,69 @@ st.sidebar.info(
     """
 )
 
-st.title("Climate Risk Imapct Assessment")
+# Main title
+st.title("Climate Risk Impact Assessment")
 
+# Layout
 col1, col2 = st.columns([4, 1])
-
 Map = geemap.Map()
 Map.add_basemap("ESA WorldCover 2020 S2 FCC")
 Map.add_basemap("ESA WorldCover 2020 S2 TCC")
 Map.add_basemap("HYBRID")
 
-# Add flood layer to the map
-Map.add_layer_control()
-Map.add_layer(flood_layers[flood_layer_choice])
-
-esa = ee.ImageCollection("ESA/WorldCover/v100").first()
-esa_vis = {"bands": ["Map"]}
-
-
-esri = ee.ImageCollection(
-    "projects/sat-io/open-datasets/landcover/ESRI_Global-LULC_10m"
-).mosaic()
-esri_vis = {
-    "min": 1,
-    "max": 10,
-    "palette": [
-        "#1A5BAB",
-        "#358221",
-        "#A7D282",
-        "#87D19E",
-        "#FFDB5C",
-        "#EECFA8",
-        "#ED022A",
-        "#EDE9E4",
-        "#F2FAFF",
-        "#C8C8C8",
-    ],
-}
-
-
-markdown = """
-    - [Dynamic World Land Cover](https://developers.google.com/earth-engine/datasets/catalog/GOOGLE_DYNAMICWORLD_V1?hl=en)
-    - [ESA Global Land Cover](https://developers.google.com/earth-engine/datasets/catalog/ESA_WorldCover_v100)
-    - [ESRI Global Land Cover](https://samapriya.github.io/awesome-gee-community-datasets/projects/esrilc2020)
-
-"""
-
+# Sidebar controls
 with col2:
-
+    # Location center and zoom
     longitude = st.number_input("Longitude", -180.0, 180.0, -89.3998)
     latitude = st.number_input("Latitude", -90.0, 90.0, 43.0886)
     zoom = st.number_input("Zoom", 0, 20, 11)
-
     Map.setCenter(longitude, latitude, zoom)
 
+    # Dynamic World time range
     start = st.date_input("Start Date for Dynamic World", datetime.date(2020, 1, 1))
     end = st.date_input("End Date for Dynamic World", datetime.date(2021, 1, 1))
-
     start_date = start.strftime("%Y-%m-%d")
     end_date = end.strftime("%Y-%m-%d")
-
     region = ee.Geometry.BBox(-179, -89, 179, 89)
     dw = geemap.dynamic_world(region, start_date, end_date, return_type="hillshade")
 
+    # Layer toggle and split map interface
     layers = {
         "Dynamic World": geemap.ee_tile_layer(dw, {}, "Dynamic World Land Cover"),
         "ESA Land Cover": geemap.ee_tile_layer(esa, esa_vis, "ESA Land Cover"),
         "ESRI Land Cover": geemap.ee_tile_layer(esri, esri_vis, "ESRI Land Cover"),
+        "Floods HP": hp_layer,
+        "Floods MP": mp_layer,
+        "Floods LP": lp_layer,
     }
 
     options = list(layers.keys())
     left = st.selectbox("Select a left layer", options, index=1)
     right = st.selectbox("Select a right layer", options, index=0)
+    Map.split_map(layers[left], layers[right])
 
-    left_layer = layers[left]
-    right_layer = layers[right]
-
-    Map.split_map(left_layer, right_layer)
-
+    # Dynamic legend for selected right layer
     legend = st.selectbox("Select a legend", options, index=options.index(right))
     if legend == "Dynamic World":
-        Map.add_legend(
-            title="Dynamic World Land Cover",
-            builtin_legend="Dynamic_World",
-        )
+        Map.add_legend(title="Dynamic World Land Cover", builtin_legend="Dynamic_World")
     elif legend == "ESA Land Cover":
         Map.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
     elif legend == "ESRI Land Cover":
         Map.add_legend(title="ESRI Land Cover", builtin_legend="ESRI_LandCover")
 
+    # Data Sources
     with st.expander("Data sources"):
-        st.markdown(markdown)
+        st.markdown("""
+        - [Dynamic World Land Cover](https://developers.google.com/earth-engine/datasets/catalog/GOOGLE_DYNAMICWORLD_V1?hl=en)
+        - [ESA Global Land Cover](https://developers.google.com/earth-engine/datasets/catalog/ESA_WorldCover_v100)
+        - [ESRI Global Land Cover](https://samapriya.github.io/awesome-gee-community-datasets/projects/esrilc2020)
+        """)
 
-
+# Show map in main panel
 with col1:
+    Map.add_layer_control()
     Map.to_streamlit(height=750)
+
 
 
 import streamlit as st
